f8a965cfc0a3d96d92d1716ad2e86be9cc34dc50
Pory Sokheng <porysokheng@gmail.com>
2021-08-17 20:25:34 +0700

    keep original extension when rename

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const path = require('path');
 const wallpaperFolder = path.join(__dirname, 'Wallpapers');
 
 fs.readdirSync(wallpaperFolder).forEach((file, i) => {
+  const extensions = file.split('.')[1];
   fs.rename(
     path.join(wallpaperFolder, file),
-    path.join(wallpaperFolder, `${i}.jpg`),
+    path.join(wallpaperFolder, `${i}.${extensions}`),
     (err) => {
       if (err) console.log(err);
     }
